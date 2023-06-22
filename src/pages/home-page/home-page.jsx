8494bcd2f3733b92e43c57b8942702051c37b035
Nde Dilan <nde.dilan@ictuniversity.edu.cm>
2023-06-22 21:26:31 +0100

    Organization of the code into component and introduction of the react router library

---
+++ src/pages/home-page/home-page.jsx
@@ -0,0 +1,12 @@
+import React from "react";  
+import './home-page.scss'
+import Directory from '../../components/directory/directory-item.jsx';
+ const HomePage = ()=>{
+    return (
+    <div className="homepage">
+        <Directory></Directory>
+    </div>
+)
+}
+
+export default HomePage;
