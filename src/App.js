f0b2dad8bd158cbf2cd560560c5e1d99cb48ab25
Nde Dilan <nde.dilan@ictuniversity.edu.cm>
2023-06-25 16:26:43 +0200

    updating our app with a shop page, a collection and collection item component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
 import './App.css';
 import HomePage from './pages/home-page/home-page.jsx'
+import ShopPage from './pages/shop-page/shop-page.jsx'
 
 import { Switch,Route } from 'react-router-dom';
 
-const HatsPage = ()=>{
-  <div className="">
-    <h1>Hats Page</h1>
-  </div>
-}
+
 function App() {
   return (
     <div >
       /*As long as one route matches is okay and you can stop there */
       <Switch>
       <Route exact path='/' component={HomePage}></Route>/*exact ,eans that the path should be exactly / before rendering */
-      <Route exact path='/hats' component={HatsPage}></Route>
+      <Route exact path='/shop' component={ShopPage}></Route>
       </Switch>
     </div>
-  );  
+  );
 }
 
 export default App;
